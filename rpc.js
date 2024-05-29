eca2fdde59767a517011c8ba98877f4837b07f30
sbw22 <sbweishaar@icloud.com>
2024-05-29 16:04:19 -0500

    added messages and buttons to html. created styles.css. cleaned up js file.

--- rpc.js
+++ rpc.js
@@ -1,3 +1,14 @@
+let rBtn = document.getElementById('rock');
+let pBtn = document.getElementById('paper');
+let sBtn = document.getElementById('scissors');
+let btns = document.getElementById('btns');
+let results = document.getElementById('results');
+let pScore = 0;
+let cScore = 0;
+let counter = 0;
+
+//  const numOfGames = prompt('How many rounds do you want?');
+
 
 const computerPlay = () => {
     let compChoice;
@@ -19,36 +30,100 @@ const personPlay = () => {
     return personChoice;
 }
 
-const playRound = () => {
-    const pChoice = personPlay();
-    const cChoice = computerPlay();
+function playRound (choice) {
 
-    if (cChoice == 'rock' && pChoice == 'scissors'){
+    //const pChoice = choice;
+    const cChoice = computerPlay();
+    
+    counter += 1;
+
+    if (cChoice == 'rock' && choice == 'scissors'){
+        cScore += 1;
         return 'You lose! Rock beats Scissors!';
-    } else if (cChoice == 'paper' && pChoice == 'rock'){
+    } else if (cChoice == 'paper' && choice == 'rock'){
+        cScore += 1;
         return 'You lose! Paper beats Rock!';
-    } else if (cChoice == 'scissors' && pChoice == 'paper'){
+    } else if (cChoice == 'scissors' && choice == 'paper'){
+        cScore += 1;
         return 'You lose! Scissors beats Rock!';
-    } else if (cChoice == 'rock' && pChoice == 'paper'){
+    } else if (cChoice == 'rock' && choice == 'paper'){
+        pScore += 1;
         return 'You win! Paper beats Rock!';
-    } else if (cChoice == 'paper' && pChoice == 'scissors'){
+    } else if (cChoice == 'paper' && choice == 'scissors'){
+        pScore += 1;
         return 'You win! Scissors beats Paper!';
-    } else if (cChoice == 'scissors' && pChoice == 'rock'){
+    } else if (cChoice == 'scissors' && choice == 'rock'){
+        pScore += 1;
         return 'You win! Rock beats Scissors!';
     } else {
         return 'tie!';
     }
 }
 
-const game = () => {
-    const numOfGames = prompt('How many rounds do you want?');
-    for (let i =0; i < numOfGames; i++) {
-        console.log(playRound());
+const addResults = (result) => {
+    if(pScore < 5 && cScore < 5) {
+        let newR = document.createElement('div');
+        newR.textContent = result + " The score is " + pScore + "-" + cScore + "."; 
+        results.appendChild(newR);
+    } else if(pScore == 5){
+        let newR = document.createElement('div');
+        newR.textContent = result; 
+        results.appendChild(newR);
+
+        let end = document.createElement('div');
+        end.textContent = "YOU WIN!!!! The score was " + pScore + "-" + cScore + ".";
+        results.appendChild(end);
+    } else{
+        let newR = document.createElement('div');
+        newR.textContent = result;
+        results.appendChild(newR);
+
+        let end = document.createElement('div');
+        end.textContent = "Computer Wins!!!! The score was " + pScore + "-" + cScore + ".";
+        results.appendChild(end);
     }
+}    
+
+rBtn.addEventListener('click', (event) =>{
+  if(/* numOfGames > counter && */ pScore < 5 && cScore < 5) {
+    addResults(playRound('rock')); 
+  }
+});
+
+pBtn.addEventListener('click', (event) =>{
+    if ( /* numOfGames > counter && */ pScore < 5 && cScore < 5) {
+        addResults(playRound('paper'));
+    }
+});
+
+sBtn.addEventListener('click', (event) =>{
+    if( /* numOfGames > counter && */ pScore < 5 && cScore < 5) {
+        addResults(playRound('scissors'));
+    }
+});
+       
+    
+
+
+
+/*
+const game = () => {
+    
+    
+    if(numOfGames >= 0) {
+        console.log('in loop');
+        rBtn.addEventListener('click', playRound);
+        pBtn.addEventListener('click', playRound());
+        sBtn.addEventListener('click', playRound());
+        
+        
+        console.log('end of loop');
+    }
+    console.log('end of game');
 }
+game(); */
 
 
-game();
 
 
 
